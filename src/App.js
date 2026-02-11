8215dfe446c02fd7aba32ceb61c0d838b86dd780
Arijbm <arijbenmarzoug1@gmail.com>
2026-02-11 15:14:38 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import LoginSignin from './components/loginSignin/loginSignin';
+
+function App() {
+  return (
+    <div>
+      <LoginSignin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/loginSignin/Forget.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Forget = () => {
+  return (
+    <div className='container'>
+        <h1>Find you</h1>
+        <div className="input">
+            <input type="email" placeholder="Email adress" />
+        </div>
+        <div className="button">
+            <button>Continue</button>
+        </div>
+    </div>
+  )
+}
+export default Forget
